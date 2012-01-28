cea55215ce16fb597d43b05b04b4d5fe97e80a7f
Vincent Lark <vincent.lark@gmail.com>
2012-01-28 17:20:36 +0100

    AudioManager

--- JumpNPuke/JNPAudioManager.m
+++ JumpNPuke/JNPAudioManager.m
@@ -20,7 +20,7 @@
         self.counter = 0;
         self.nextMusicStress = 0;
 
-        [self schedule:@selector(backgroundMusicTick:) interval:0.5];
+        [self schedule:@selector(backgroundMusicTick:) interval:0.84];
     }
    	return self;
 }
@@ -29,21 +29,27 @@
 -(void) playMusic:(int)stress {
     switch (stress) {
         case 1:
-            [[SimpleAudioEngine sharedEngine] playBackgroundMusic:@"Tribal.mp3" loop:YES];
+            [[SimpleAudioEngine sharedEngine] playBackgroundMusic:@"Tribal_1.mp3" loop:YES];
             break;
-            
         case 2:
             [[SimpleAudioEngine sharedEngine] playBackgroundMusic:@"Tribal_2.mp3" loop:YES];
             break;
-
+        case 3:
+            [[SimpleAudioEngine sharedEngine] playBackgroundMusic:@"Tribal_3.mp3" loop:YES];
+            break;
+        case 4:
+            [[SimpleAudioEngine sharedEngine] playBackgroundMusic:@"Tribal_4.mp3" loop:YES];
+            break;
+        case 5:
+            [[SimpleAudioEngine sharedEngine] playBackgroundMusic:@"Tribal_5.mp3" loop:YES];
+            break;
         default:
-        case 0:
             break;
     }
 }
 
 // playNextMusic
--(void) playMusicNext:(int)stress {
+-(void) playMusicWithStress:(int)stress {
     self.nextMusicStress = stress;
 }
 
@@ -53,15 +59,12 @@
         case jnpSndDummy:
             [[SimpleAudioEngine sharedEngine] playEffect:@"gameboy-startup.wav"];
             break;
-            
         case jnpSndJump:
             [[SimpleAudioEngine sharedEngine] playEffect:@"jump.mp3"];
             break;
-            
         case jnpSndDie:
             [[SimpleAudioEngine sharedEngine] playEffect:@"die.mp3"];
             break;
-            
         default:
             break;
     }
@@ -70,17 +73,19 @@
 // preload files
 -(void) preload {
     [[SimpleAudioEngine sharedEngine] preloadBackgroundMusic:@"calm.mp3"];
-    [[SimpleAudioEngine sharedEngine] preloadBackgroundMusic:@"Tribal.mp3"];
+    [[SimpleAudioEngine sharedEngine] preloadBackgroundMusic:@"calm.mp3"];
+    [[SimpleAudioEngine sharedEngine] preloadBackgroundMusic:@"Tribal_1.mp3"];
     [[SimpleAudioEngine sharedEngine] preloadBackgroundMusic:@"Tribal_2.mp3"];
-    [[SimpleAudioEngine sharedEngine] preloadEffect:@"music1.mp3"];
-    [[SimpleAudioEngine sharedEngine] preloadEffect:@"music2.mp3"];
+    [[SimpleAudioEngine sharedEngine] preloadBackgroundMusic:@"Tribal_3.mp3"];
+    [[SimpleAudioEngine sharedEngine] preloadBackgroundMusic:@"Tribal_4.mp3"];
+    [[SimpleAudioEngine sharedEngine] preloadBackgroundMusic:@"Tribal_5.mp3"];
 }
 
 // called every 0.5 sec
 -(void) backgroundMusicTick:(ccTime)time {
     self.counter = self.counter + 1;
     
-    if (self.counter % 12 == 0 && self.nextMusicStress != 0) {
+    if (self.counter % 10 == 0 && self.nextMusicStress != 0) {
         NSLog(@"time has come to play next music with stress %d", self.nextMusicStress);
         int stress = self.nextMusicStress;
         self.nextMusicStress = 0;
