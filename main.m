d7e33fc1a97fd52c2c2b5b7c4795045a5ff31287
Vincent Lark <vincent.lark@gmail.com>
2012-01-28 08:28:34 +0100

    initial import

---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Test22
+//
+//  Created by Vincent on 28/01/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
